74cce7bfe9474d70537de5cbbd9e69b27eddd66c
Auguste Dutcher <Aug@MacBook-Pro.local>
2017-07-18 12:13:16 -0700

    initial commit

---
+++ split_fasta.py
@@ -0,0 +1,18 @@
+import sys
+from Bio import SeqIO, SeqRecord
+
+def split_fasta(multifasta):
+    with open(multifasta, 'r') as infile:
+        records = SeqIO.parse(infile, 'fasta')
+        for record in records:
+            filename = record.id + '.fa'
+            out_handle = open(filename, 'w')
+            SeqIO.write(record, out_handle, 'fasta')
+            out_handle.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+         split_fasta(sys.argv[1])
+    else:
+         print("Usage: split_fasta.py multifasta.fa")
+         sys.exit(0)
